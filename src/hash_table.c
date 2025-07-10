fa500f6db291fdc07081661f52603cc6edd1ab24
xaizko <tonytran177013@gmail.com>
2025-07-10 19:48:23 -0400

    added hash table init function

--- src/hash_table.c
+++ src/hash_table.c
@@ -2,6 +2,20 @@
 #include <string.h>
 #include "hash_table.h"
 
+#define HT_DEFAULT_SIZE 53
+
+//initialize hash table
+ht_hash_table *ht_init() {
+    ht_hash_table *ht = malloc(sizeof(ht_hash_table));
+
+    ht->size = HT_DEFAULT_SIZE;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+    
+    return ht;
+}
+
+//add item to hash table
 static ht_item *ht_add_item(const char *k, const char *v) {
     ht_item *i = malloc(sizeof(ht_item));
     i->key = strdup(k);
--- src/hash_table.h
+++ src/hash_table.h
@@ -10,5 +10,8 @@ typedef struct {
     ht_item **items;
 } ht_hash_table;
 
+//function to initialize hash table
+ht_hash_table *ht_init();
+
 //function to add item to hash table
 static ht_item *ht_add_item(const char *k, const char *v);
