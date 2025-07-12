e980631de981a89dd4b47158289a2ff2f875a8f4
xaizko <tonytran177013@gmail.com>
2025-07-12 08:07:03 -0400

    added insert function

--- src/hash_table.c
+++ src/hash_table.c
@@ -66,3 +66,18 @@ static int ht_get_hash(const char *s, int num_buckets, const int attempt) {
     const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
+
+//insert into hash table
+void ht_insert(ht_hash_table *ht, const char *key, const char *value) {
+    ht_item *item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item *cur_item = ht->items[index];
+    int i = 1;
+    while (cur_item != NULL) {
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
--- src/hash_table.h
+++ src/hash_table.h
@@ -27,3 +27,6 @@ static int ht_hash(char *s, const int a, const int m);
 
 //function to double hash to avoid collision
 static int ht_get_hash(const char *s, int num_buckets, const int attempt);
+
+//function to insert into hash table
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
