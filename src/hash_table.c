9a384b7fd36345498e37fd417040954cbb1c9271
xaizko <tonytran177013@gmail.com>
2025-07-13 11:40:47 -0400

    updated ht_insert and ht_search to account for deleted items

--- src/hash_table.c
+++ src/hash_table.c
@@ -76,7 +76,7 @@ void ht_insert(ht_hash_table *ht, const char *key, const char *value) {
     int index = ht_get_hash(item->key, ht->size, 0);
     ht_item *cur_item = ht->items[index];
     int i = 1;
-    while (cur_item != NULL) {
+    while (cur_item != NULL && cur_item != &HT_DELETED_ITEM) {
         index = ht_get_hash(item->key, ht->size, i);
         cur_item = ht->items[index];
         i++;
@@ -90,8 +90,10 @@ char *ht_search(ht_hash_table *ht, const char *key) {
     ht_item *item = ht->items[index];
     int i = 1;
     while (item != NULL) {
-        if (!strcmp(item->key, key)) {
-            return item->value;
+        if (item != &HT_DELETED_ITEM) {
+            if (!strcmp(item->key, key)){
+                return item->value;
+            }
         }
         index = ht_get_hash(key, ht->size, i);
         item = ht->items[index];
